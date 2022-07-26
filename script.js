f4ab1aa0eb7027d4871963aecfd762e069103cc0
e-meyer <edmundom@mx2.unisc.br>
2022-07-26 14:39:50 -0300

    Adding background for each row

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 let grid = document.querySelector('.grid')
 
+/*
+    >> FUNCTION START
+        >> LOOP 16 TIMES TO MAKE A ROW
+        >> BREAK LINE
+        >> REPEAT UNTIL THERE'S 16 ROWS
+
+
+*/
 
 let gridMaker = () => {
     for(let i=0;i<16;i++){
+        let newDiv1 = document.createElement('div')
+        let newDiv = document.createElement('div')
         for(let j=0;j<16;j++){
-            let newDiv = document.createElement('div')
-            newDiv.style.border = '1px solid #cecece'
-            newDiv.style.padding = '16px'
+            newDiv.style.border = '1px solid #d9d9d9'
+            newDiv.style.padding = '20px'
+            newDiv.style.background = '#252525'
             newDiv.classList.add('grid-square')
-            grid.appendChild(newDiv)
         }
+        newDiv1.appendChild(newDiv)
+        grid.appendChild(newDiv1)
     }
+    
+    
 }
 
 gridMaker();
+
+let squares = Array.from(document.getElementsByClassName('grid-square'))
+
+squares.forEach((button) => {
+    button.addEventListener('click', () => {
+        button.style.background = '#d9d9d9'
+    })
+})
+
+squares.forEach((button) => {
+    button.addEventListener('mousemove', () => {
+        button.style.background = '#d9d9d9'
+    })
+})
