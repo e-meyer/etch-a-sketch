73bc85c272f60f77026af5fa2dcc02ec531a6556
e-meyer <edmundom@mx2.unisc.br>
2022-07-25 22:46:06 -0300

    Add border to grid

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let grid = document.querySelector('.grid')
 
+
 let gridMaker = () => {
     for(let i=0;i<16;i++){
         for(let j=0;j<16;j++){
             let newDiv = document.createElement('div')
+            newDiv.style.border = '1px solid #cecece'
             newDiv.style.padding = '16px'
             newDiv.classList.add('grid-square')
             grid.appendChild(newDiv)
