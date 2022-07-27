001595951216417aa604627935a2e1fcf42df8ca
e-meyer <edmundom@mx2.unisc.br>
2022-07-26 21:25:58 -0300

    Change background

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 let grid = document.querySelector('.grid')
 let toolSelected = Array.from(document.querySelectorAll('.brush-tool, .eraser'))
 
-console.log(toolSelected[0].className)
-console.log(toolSelected[1].className)
-
 let gridMaker = () => {
     for(let i=0;i<16;i++){
         for(let j=0;j<16;j++){
-            let newDiv = document.createElement('div')
-            newDiv.style.border = '1px solid #565656'
-            newDiv.style.padding = '20px'
-            newDiv.style.background = '#252525'
-            newDiv.classList.add('grid-square')
-            grid.appendChild(newDiv)
+            let gridSquare = document.createElement('div')
+            gridSquare.style.border = '1px solid #565656'
+            gridSquare.style.padding = '20px'
+            gridSquare.style.background = '#252525'
+            gridSquare.classList.add('grid-square')
+            grid.appendChild(gridSquare)
         }
     }
-    
-    
 }
 
 gridMaker();
@@ -26,8 +21,8 @@ let squares = Array.from(document.getElementsByClassName('grid-square'))
 squares.forEach((button) => {
     button.addEventListener('click', () => {
         
-        if(toolSelected[0].classList.contains('selected')) button.style.background = '#d9d9d9'
-        else button.style.background = '#252525'
+        if(toolSelected[0].classList.contains('selected')) button.style.background = '#d9d9d9'  // if brush tool is selected
+        else button.style.background = '#252525'                                                // if eraser is selected
     })
 })
 
@@ -37,11 +32,14 @@ toolSelected.forEach((tool) => {
 
         if(tool.classList.contains('brush-tool')){
             toolSelected[1].classList.remove('selected')
-            tool.classList.add('selected')
+            toolSelected[1].style.background = '#264a70'
         } else {
             toolSelected[0].classList.remove('selected')
-            tool.classList.add('selected')
+            toolSelected[0].style.background = '#264a70'
         }
         
+        tool.classList.add('selected')
+        tool.style.background = '#9DC3ED'
+
     })
 })
