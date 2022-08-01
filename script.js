c62a88b784ad64c2de553d47d6275e4e67e3e744
e-meyer <edmundom@mx2.unisc.br>
2022-08-01 15:12:49 -0300

    Better looking code, remove unused functions, added some perks

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
 let grid = document.querySelector('.grid')
 let toolSelected = Array.from(document.querySelectorAll('.brush-tool, .eraser'))
 let gridSize = document.querySelector('.grid-size')
-let slider = document.querySelector('#slider')
-let sliderRange = document.querySelector('#slider').value
+let sliderGrid = document.querySelector('#slider')
+let sliderValue = document.querySelector('#slider').value
 let clearButton = document.querySelector('.clear')
 let colorPicker = document.querySelector('.color-picker')
-let color = document.querySelector('.color-picker').value
+let colorValue = document.querySelector('.color-picker').value
 
 let mouseDown = false
 document.body.onmousedown = () => {mouseDown = true}
 document.body.onmouseup = () => {mouseDown = false}
 
-colorPicker.addEventListener('change', () => {
-    color = document.querySelector('.color-picker').value
-    console.log(color)
-})
-
-let gridMaker = () => {
+function gridMaker() {
     clearGrid()
-    sliderRange = document.querySelector('#slider').value
-    grid.style.gridTemplateColumns = `repeat(${sliderRange}, 1fr)`
-    grid.style.gridTemplateRows = `repeat(${sliderRange}, 1fr)`
-    gridSize.innerText = document.querySelector('#slider').value + ' x ' +  document.querySelector('#slider').value
-    for(let i=0;i<sliderRange * sliderRange;i++){
+    sliderValue = document.querySelector('#slider').value
+    grid.style.gridTemplateColumns = `repeat(${sliderValue}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${sliderValue}, 1fr)`
+    gridSize.innerText = `${sliderValue} x ${sliderValue}`
+
+    for(let i = 0; i < (sliderValue * sliderValue); i++){
         let gridSquare = document.createElement('div')
-        if(sliderRange <= 48) gridSquare.style.border = '1px solid #565656';
+        if(sliderValue <= 48) gridSquare.style.border = '1px solid #565656';
         gridSquare.addEventListener('mouseover', changeColor)
         gridSquare.addEventListener('mousedown', changeColor)
         gridSquare.classList.add('grid-square')
@@ -32,31 +28,11 @@ let gridMaker = () => {
     }
 }
 
-function changeColor(e) {
-    if (e.type === 'mouseover' && !mouseDown)return
-    else if (toolSelected[0].classList.contains('selected')) e.target.style.background = '#d9d9d9'
-    else e.target.style.background = '#252525'
-}
+window.onload = () => gridMaker();
 
-function clearGrid() {
-    grid.innerHTML = ''
-}
-
-gridMaker();
-
-let squares = Array.from(document.getElementsByClassName('grid-square'))
-
-// squares.forEach((button) => {
-//     button.addEventListener('mousemove', () => {
-        
-//         if(toolSelected[0].classList.contains('selected')) button.style.background = '#d9d9d9'  // if brush tool is selected
-//         else button.style.background = '#252525'                                                // if eraser is selected
-//     })
-// })
-
-slider.addEventListener('change', () => {
-    gridMaker();
-})
+colorPicker.onchange = () => colorValue = document.querySelector('.color-picker').value
+sliderGrid.onchange = () => gridMaker()
+clearButton.onclick = () => gridMaker()
 
 toolSelected.forEach((tool) => {
     tool.addEventListener('click', () => {
@@ -76,6 +52,12 @@ toolSelected.forEach((tool) => {
     })
 })
 
-clearButton.onclick = () => {
-    gridMaker()
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    else if (toolSelected[0].classList.contains('selected')) e.target.style.background = colorValue
+    else e.target.style.background = '#252525'
+}
+
+function clearGrid() {
+    grid.innerHTML = ''
 }
