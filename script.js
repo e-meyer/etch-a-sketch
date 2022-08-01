9fca1cf57c3a1140ed2a02074b1a9a4f81ccfce4
e-meyer <edmundom@mx2.unisc.br>
2022-08-01 14:57:04 -0300

    Create color input, clear button and fixed bugs

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ let toolSelected = Array.from(document.querySelectorAll('.brush-tool, .eraser'))
 let gridSize = document.querySelector('.grid-size')
 let slider = document.querySelector('#slider')
 let sliderRange = document.querySelector('#slider').value
+let clearButton = document.querySelector('.clear')
+let colorPicker = document.querySelector('.color-picker')
+let color = document.querySelector('.color-picker').value
+
+let mouseDown = false
+document.body.onmousedown = () => {mouseDown = true}
+document.body.onmouseup = () => {mouseDown = false}
+
+colorPicker.addEventListener('change', () => {
+    color = document.querySelector('.color-picker').value
+    console.log(color)
+})
 
 let gridMaker = () => {
     clearGrid()
@@ -12,10 +24,18 @@ let gridMaker = () => {
     gridSize.innerText = document.querySelector('#slider').value + ' x ' +  document.querySelector('#slider').value
     for(let i=0;i<sliderRange * sliderRange;i++){
         let gridSquare = document.createElement('div')
-        
+        if(sliderRange <= 48) gridSquare.style.border = '1px solid #565656';
+        gridSquare.addEventListener('mouseover', changeColor)
+        gridSquare.addEventListener('mousedown', changeColor)
         gridSquare.classList.add('grid-square')
         grid.appendChild(gridSquare)
     }
+}
+
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown)return
+    else if (toolSelected[0].classList.contains('selected')) e.target.style.background = '#d9d9d9'
+    else e.target.style.background = '#252525'
 }
 
 function clearGrid() {
@@ -55,3 +75,7 @@ toolSelected.forEach((tool) => {
 
     })
 })
+
+clearButton.onclick = () => {
+    gridMaker()
+}
