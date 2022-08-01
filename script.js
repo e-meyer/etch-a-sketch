3ecc8911fba3d01894a19aabf817e3bd3e310c14
e-meyer <edmundom@mx2.unisc.br>
2022-08-01 12:37:17 -0300

    Change display style to grid, remove color functions for tests, add grid slider

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
 let grid = document.querySelector('.grid')
 let toolSelected = Array.from(document.querySelectorAll('.brush-tool, .eraser'))
+let gridSize = document.querySelector('.grid-size')
+let slider = document.querySelector('#slider')
+let sliderRange = document.querySelector('#slider').value
 
 let gridMaker = () => {
-    for(let i=0;i<16;i++){
-        for(let j=0;j<16;j++){
-            let gridSquare = document.createElement('div')
-            gridSquare.style.border = '1px solid #565656'
-            gridSquare.style.padding = '20px'
-            gridSquare.style.background = '#252525'
-            gridSquare.classList.add('grid-square')
-            grid.appendChild(gridSquare)
-        }
+    clearGrid()
+    sliderRange = document.querySelector('#slider').value
+    grid.style.gridTemplateColumns = `repeat(${sliderRange}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${sliderRange}, 1fr)`
+    gridSize.innerText = document.querySelector('#slider').value + ' x ' +  document.querySelector('#slider').value
+    for(let i=0;i<sliderRange * sliderRange;i++){
+        let gridSquare = document.createElement('div')
+        
+        gridSquare.classList.add('grid-square')
+        grid.appendChild(gridSquare)
     }
+}
+
+function clearGrid() {
+    grid.innerHTML = ''
 }
 
 gridMaker();
 
 let squares = Array.from(document.getElementsByClassName('grid-square'))
 
-squares.forEach((button) => {
-    button.addEventListener('mousemove', () => {
+// squares.forEach((button) => {
+//     button.addEventListener('mousemove', () => {
         
-        if(toolSelected[0].classList.contains('selected')) button.style.background = '#d9d9d9'  // if brush tool is selected
-        else button.style.background = '#252525'                                                // if eraser is selected
-    })
+//         if(toolSelected[0].classList.contains('selected')) button.style.background = '#d9d9d9'  // if brush tool is selected
+//         else button.style.background = '#252525'                                                // if eraser is selected
+//     })
+// })
+
+slider.addEventListener('change', () => {
+    gridMaker();
 })
 
 toolSelected.forEach((tool) => {
